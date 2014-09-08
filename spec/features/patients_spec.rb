5cac13ea705750368edc04eeb7d4818906325c31
Sean Smith <seansmith1020@gmail.com>
2014-09-08 11:09:53 -0600

    test

--- spec/features/patients_spec.rb
+++ spec/features/patients_spec.rb
@@ -31,7 +31,7 @@ feature "Patients" do
 
     click_link "Add Prescription"
 
-    select "Tylenol", from: "prescription[medication]"
+    select "Tylenol", from: "prescription[medication_id]"
     fill_in "prescription[dosage]", with: "5 mgs"
     fill_in "prescription[schedule]", with: "2 days"
     fill_in "prescription[start]", with: "09/26/2014"
@@ -62,6 +62,32 @@ feature "Patients" do
     expect(page).to have_content("Schedule can't be blank")
     expect(page).to have_content("Start can't be blank")
     expect(page).to have_content("End can't be blank")
+  end
+
+  scenario "Viewing med show page displays info on all prescrips written for that med" do
+    user = create_user
+    patient = create_patient
+    medication = create_medication
+    login(user)
+    click_link "#{patient.first_name} #{patient.last_name}"
+
+    click_link "Add Prescription"
+
+    select "Tylenol", from: "prescription[medication_id]"
+    fill_in "prescription[dosage]", with: "5 mgs"
+    fill_in "prescription[schedule]", with: "2 days"
+    fill_in "prescription[start]", with: "09/26/2014"
+    fill_in "prescription[end]", with: "09/27/2014"
+
+    click_button "Create Prescription"
+
+    visit '/'
+
+    click_link "Tylenol"
+
+    expect(page).to have_content ("#{patient.first_name} #{patient.last_name}")
+    expect(page).to have_content ("5 mgs every 2 days")
+    expect(page).to have_content ("2014-09-26 - 2014-09-27")
   end
 
 end
